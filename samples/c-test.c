c05e36dd57a465155927df96fbe25a4061c40146
Ingo Wald <ingowald@gmail.com>
2019-11-18 13:23:08 -0700

    first iteration with cleaned-up direcotry structure - compiles, but missing api funcs

--- samples/c-test.c
+++ samples/c-test.c
@@ -1,4 +1,20 @@
-#include "c-api.h"
+// ======================================================================== //
+// Copyright 2019 Ingo Wald                                                 //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+// ======================================================================== //
+
+#include "owl/owl.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -187,7 +203,7 @@ int main(int ac, char **av)
                                  /* code to run: */
                                  module,"renderFrame",
                                  /* size of variables struct */
-                                 sizeof(RenderFrameVars));
+                                 sizeof(struct RenderFrameVars));
 
   OWLVariable bgColor
     = owlLaunchProgGetVariable(renderFrame,"bgColor");
