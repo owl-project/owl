4b4587f3f216e14ef34003f3be55860ec1dd7caa
Ingo Wald <ingowald@gmail.com>
2020-01-16 18:00:49 -0700

    added file and lineback into 'ping'

--- owl/common/owl-common.h
+++ owl/common/owl-common.h
@@ -87,7 +87,7 @@
 #ifdef __WIN32__
 # define PING std::cout << __FILE__ << "::" << __LINE__ << ": " << __FUNCTION__ << std::endl;
 #else
-# define PING std::cout << __PRETTY_FUNCTION__ << std::endl;
+# define PING std::cout << __FILE__ << "::" << __LINE__ << ": " << __PRETTY_FUNCTION__ << std::endl;
 #endif
 #endif
 
