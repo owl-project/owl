8bf1d3841ffbce18f3f51fe92e9cb9711f281d50
Ingo Wald <ingowald@gmail.com>
2020-05-16 13:29:14 -0600

    splitting off advanced samples

--- scripts/globalSearchAndReplace.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-allFiles=`find samples/ -name '*cu' -or -name '*.h' -or -name '*cpp'`
-for f in $allFiles; do
-    echo $f
-    cat $f | sed "s/GDT/OWL/g" > tmp.snr
-    cp tmp.snr $f
-    rm tmp.snr
-done
