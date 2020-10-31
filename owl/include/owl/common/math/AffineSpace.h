b0563efc50418cdf430218cff4d43889721aea3d
Ingo Wald <ingowald@gmail.com>
2020-10-30 20:04:19 -0600

    rcp(AffineSpace) now also works on __device__

--- owl/include/owl/common/math/AffineSpace.h
+++ owl/include/owl/common/math/AffineSpace.h
@@ -115,7 +115,12 @@ namespace owl {
 
     template<typename L> inline AffineSpaceT<L> operator -( const AffineSpaceT<L>& a ) { return AffineSpaceT<L>(-a.l,-a.p); }
     template<typename L> inline AffineSpaceT<L> operator +( const AffineSpaceT<L>& a ) { return AffineSpaceT<L>(+a.l,+a.p); }
-    template<typename L> inline __both__ AffineSpaceT<L>        rcp( const AffineSpaceT<L>& a ) { L il = rcp(a.l); return AffineSpaceT<L>(il,-(il*a.p)); }
+    template<typename L>
+    inline __both__
+    AffineSpaceT<L> rcp( const AffineSpaceT<L>& a ) {
+      L il = rcp(a.l);
+      return AffineSpaceT<L>(il,-(il*a.p));
+    }
 
     ////////////////////////////////////////////////////////////////////////////////
     // Binary Operators
