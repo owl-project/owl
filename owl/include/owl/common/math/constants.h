98b0193ba71f84bbe04606130994885ca92920a1
Ingo Wald <ingowald@gmail.com>
2020-11-24 12:45:48 -0700

    more fleshing out of bool, char, byte types

--- owl/include/owl/common/math/constants.h
+++ owl/include/owl/common/math/constants.h
@@ -17,6 +17,7 @@
 #pragma once
 
 #include <limits>
+#include <limits.h>
 #ifdef __CUDACC__
 #include <math_constants.h>
 #endif
@@ -182,22 +183,40 @@ namespace owl {
     }
 
     template<> inline __both__ uint16_t empty_range_lower<uint16_t>() 
-    { return uint16_t(-1); }
+    { return uint16_t(USHRT_MAX); }
     template<> inline __both__ uint16_t empty_range_upper<uint16_t>() 
     { return uint16_t(0); }
     template<> inline __both__ uint16_t open_range_lower<uint16_t>() 
     { return uint16_t(0); }
     template<> inline __both__ uint16_t open_range_upper<uint16_t>() 
-    { return uint16_t(-1); }
+    { return uint16_t(USHRT_MAX); }
+
+    template<> inline __both__ int16_t empty_range_lower<int16_t>() 
+    { return int16_t(SHRT_MAX); }
+    template<> inline __both__ int16_t empty_range_upper<int16_t>() 
+    { return int16_t(SHRT_MIN); }
+    template<> inline __both__ int16_t open_range_lower<int16_t>() 
+    { return int16_t(SHRT_MIN); }
+    template<> inline __both__ int16_t open_range_upper<int16_t>() 
+    { return int16_t(SHRT_MAX); }
 
     template<> inline __both__ uint8_t empty_range_lower<uint8_t>() 
-    { return uint8_t(-1); }
+    { return uint8_t(CHAR_MAX); }
     template<> inline __both__ uint8_t empty_range_upper<uint8_t>() 
-    { return uint8_t(0); }
+    { return uint8_t(CHAR_MIN); }
     template<> inline __both__ uint8_t open_range_lower<uint8_t>() 
-    { return uint8_t(0); }
+    { return uint8_t(CHAR_MIN); }
     template<> inline __both__ uint8_t open_range_upper<uint8_t>() 
-    { return uint8_t(-1); }
+    { return uint8_t(CHAR_MAX); }
+
+    template<> inline __both__ int8_t empty_range_lower<int8_t>() 
+    { return int8_t(SCHAR_MIN); }
+    template<> inline __both__ int8_t empty_range_upper<int8_t>() 
+    { return int8_t(SCHAR_MAX); }
+    template<> inline __both__ int8_t open_range_lower<int8_t>() 
+    { return int8_t(SCHAR_MAX); }
+    template<> inline __both__ int8_t open_range_upper<int8_t>() 
+    { return int8_t(SCHAR_MIN); }
 
   } // ::owl::common
 } // ::owl
