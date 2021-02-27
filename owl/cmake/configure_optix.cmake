63c04abd185e46b1fc911333feeb4814b41174f5
Louis Pisha <lpisha@eng.ucsd.edu>
2021-02-27 14:59:22 -0800

    Draft of modern and intermediate cmake implementation

--- owl/cmake/configure_optix.cmake
+++ owl/cmake/configure_optix.cmake
@@ -23,49 +23,55 @@ include(configure_cuda)
 #find_package(CUDA REQUIRED)
 find_package(OptiX REQUIRED VERSION 7)
 
-include_directories(${CUDA_TOOLKIT_INCLUDE})
+if(OWL_DEPRECATED_CMAKE)
+	include_directories(${CUDA_TOOLKIT_INCLUDE})
+endif()
 include_directories(${OptiX_INCLUDE})
 
 if (WIN32)
   add_definitions(-DNOMINMAX)
 endif()
 
+if(OWL_DEPRECATED_CMAKE)
   
-find_program(BIN2C bin2c
-  DOC "Path to the cuda-sdk bin2c executable.")
+	find_program(BIN2C bin2c
+	  DOC "Path to the cuda-sdk bin2c executable.")
 
-# this macro defines cmake rules that execute the following four steps:
-# 1) compile the given cuda file ${cuda_file} to an intermediary PTX file
-# 2) use the 'bin2c' tool (that comes with CUDA) to
-#    create a second intermediary (.c-)file which defines a const string variable
-#    (named '${c_var_name}') whose (constant) value is the PTX output
-#    from the previous step.
-# 3) compile the given .c file to an intermediary object file (why thus has
-#    that PTX string 'embedded' as a global constant.
-# 4) assign the name of the intermediary .o file to the cmake variable
-#    'output_var', which can then be added to cmake targets.
-macro(cuda_compile_and_embed output_var cuda_file)
-  set(c_var_name ${output_var})
-  if(${CMAKE_BUILD_TYPE} MATCHES "Release")
-    cuda_compile_ptx(ptx_files
-      ${cuda_file}
-      OPTIONS -O3 -DNDEBUG=1 --use_fast_math
-      )
-  else()
-    cuda_compile_ptx(ptx_files
-      ${cuda_file}
-      )
-  endif()
-  list(GET ptx_files 0 ptx_file)
-  set(embedded_file ${ptx_file}_embedded.c)
-#  message("adding rule to compile and embed ${cuda_file} to \"const char ${var_name}[];\"")
-  add_custom_command(
-    OUTPUT ${embedded_file}
-    COMMAND ${BIN2C} -c --padd 0 --type char --name ${c_var_name} ${ptx_file} > ${embedded_file}
-    DEPENDS ${ptx_file}
-    COMMENT "compiling (and embedding ptx from) ${cuda_file}"
-    )
-  set(${output_var} ${embedded_file})
-endmacro()
+	# this macro defines cmake rules that execute the following four steps:
+	# 1) compile the given cuda file ${cuda_file} to an intermediary PTX file
+	# 2) use the 'bin2c' tool (that comes with CUDA) to
+	#    create a second intermediary (.c-)file which defines a const string variable
+	#    (named '${c_var_name}') whose (constant) value is the PTX output
+	#    from the previous step.
+	# 3) compile the given .c file to an intermediary object file (why thus has
+	#    that PTX string 'embedded' as a global constant.
+	# 4) assign the name of the intermediary .o file to the cmake variable
+	#    'output_var', which can then be added to cmake targets.
+	macro(cuda_compile_and_embed output_var cuda_file)
+	  set(c_var_name ${output_var})
+	  if(${CMAKE_BUILD_TYPE} MATCHES "Release")
+		cuda_compile_ptx(ptx_files
+		  ${cuda_file}
+		  OPTIONS -O3 -DNDEBUG=1 --use_fast_math
+		  )
+	  else()
+		cuda_compile_ptx(ptx_files
+		  ${cuda_file}
+		  )
+	  endif()
+	  list(GET ptx_files 0 ptx_file)
+	  set(embedded_file ${ptx_file}_embedded.c)
+	#  message("adding rule to compile and embed ${cuda_file} to \"const char ${var_name}[];\"")
+	  add_custom_command(
+		OUTPUT ${embedded_file}
+		COMMAND ${BIN2C} -c --padd 0 --type char --name ${c_var_name} ${ptx_file} > ${embedded_file}
+		DEPENDS ${ptx_file}
+		COMMENT "compiling (and embedding ptx from) ${cuda_file}"
+		)
+	  set(${output_var} ${embedded_file})
+	endmacro()
 
+else()
+	include(embed_ptx)
+endif()
 
---
+++ owl/cmake/embed_ptx.cmake
@@ -0,0 +1,130 @@
+# Copyright (C) 2020 NVIDIA Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# embed_ptx.cmake
+# ^^^^^^^^^^^^^^^
+# Compiles a CUDA source file to PTX, and creates a C file containing a
+# character array of the text of the PTX.
+# 
+# Architectures and flags: If EMBED_PTX_ARCH is set (should be set to a value
+# like e.g. 61 or 86), generates code for just this architecture. Otherwise,
+# if CMAKE_CUDA_ARCHITECTURES is set, uses the oldest (lowest-numbered)
+# architecture set in CMAKE_CUDA_ARCHITECTURES. Otherwise, it will issue a
+# warning and use the NVCC default. 
+# Flags are propagated from CMAKE_CUDA_FLAGS, and includes are also propagated
+# from the current source directory.
+# 
+# @param c_embed_name This argument serves two functions.
+# First, it is the name of the character array containing the PTX source.
+# Second, a CMake variable by this name will be set by the function, containing
+# the path to the generated C file.
+# 
+# @param cu_file The literal name of the input CUDA file, relative to the
+# current source directory.
+# 
+# Example:
+# embed_ptx(myPtxProgram my_program.cu)
+# add_executable(foo a.c b.cu c.cpp ${myPtxProgram})
+# 
+# WARNING: embed_ptx MUST be called in the same directory (CMakeLists.txt) as
+# the add_executable or add_library call which ${myPtxProgram} is being included
+# in. Adding it later, e.g. with target_sources from a child directory, will NOT
+# work. This is a limitation of CMake, see https://stackoverflow.com/questions/
+# 57824263/cmake-doesnt-recognize-custom-command-as-valid-source
+# 
+# WARNING: Your CMake top-level project MUST include the C language, otherwise
+# CMake will never emit rules to build the generated C file into an object file
+# and you will get "undefined reference to myPtxProgram" or "unresolved external
+# symbol myPtxProgram" from the linker.
+
+function(embed_ptx c_embed_name cu_file)
+  # Initial setup
+  set(ptxfile "${CMAKE_CURRENT_BINARY_DIR}/${c_embed_name}.ptx")
+  set(embedfile "${CMAKE_CURRENT_BINARY_DIR}/${c_embed_name}.c")
+  file(RELATIVE_PATH cu_file_frombin ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/${cu_file})
+  # Find bin2c
+  if(NOT BIN2C)
+    get_filename_component(cudabindir ${CMAKE_CUDA_COMPILER} DIRECTORY)
+    if(WIN32)
+      set(BIN2C "${cudabindir}/bin2c.exe")
+    else()
+      set(BIN2C "${cudabindir}/bin2c")
+    endif()
+    if(NOT EXISTS ${BIN2C})
+      message(FATAL_ERROR "Could not find bin2c at ${BIN2C}!")
+    endif()
+  endif()
+  # Architecture
+  if(EMBED_PTX_ARCH)
+    set(archflags "-gencode arch=compute_${EMBED_PTX_ARCH},code=compute_${EMBED_PTX_ARCH}")
+  elseif(CMAKE_CUDA_ARCHITECTURES)
+    set(archflags ${CMAKE_CUDA_ARCHITECTURES})
+    list(SORT archflags CASE INSENSITIVE)
+    list(GET archflags 0 archflags)
+    string(REPLACE "-real" "" archflags ${archflags})
+    string(REPLACE "-virtual" "" archflags ${archflags})
+    set(archflags "-gencode arch=compute_${archflags},code=compute_${archflags}")
+  else()
+    set(archflags "")
+    message(WARNING "embed_ptx: no EMBED_PTX_ARCH or CMAKE_CUDA_ARCHITECTURES specified, using compiler default")
+  endif()
+  # Flags manipulation
+  string(REGEX REPLACE " -gencode arch=[A-Za-z0-9_,=]+" "" manualcudaflags "${CMAKE_CUDA_FLAGS}")
+  while(manualcudaflags MATCHES ".*\".*")
+    if(NOT manualcudaflags MATCHES ".*\".*\".*")
+      message(FATAL_ERROR "CUDA flags for embed PTX only contain one quotation mark: ${manualcudaflags}")
+    endif()
+    string(REGEX REPLACE "[^\"]*\"([^\"]*)\".*" "\\1" quotedcontents "${manualcudaflags}")
+    string(REGEX REPLACE "[^\"]* ([-A-Za-z]*)=? ?\".*" "\\1" quoteprefix "${manualcudaflags}")
+    string(REGEX REPLACE "([^\"]*) [-A-Za-z]*=? ?\".*" "\\1" beforequotes "${manualcudaflags}")
+    string(REGEX REPLACE "[^\"]*\"[^\"]*\"(.*)" "\\1" afterquotes "${manualcudaflags}")
+    string(STRIP "${quotedcontents}" quotedcontents)
+    string(STRIP "${quoteprefix}" quoteprefix)
+    string(REPLACE " " ";" quotedargs "${quotedcontents}")
+    list(TRANSFORM quotedargs PREPEND "${quoteprefix} ")
+    string(REPLACE ";" " " quotedargs "${quotedargs}")
+    set(manualcudaflags "${beforequotes} ${quotedargs} ${afterquotes}")
+  endwhile()
+  # Includes manipulation
+  get_property(includes DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY INCLUDE_DIRECTORIES)
+  if(WIN32)
+    list(TRANSFORM includes REPLACE " " "\\\\ ")
+  endif()
+  list(TRANSFORM includes PREPEND "-I ")
+  string(REPLACE ";" " " includes "${includes}")
+  # Final flags manipulation
+  set(allcudaflags "${manualcudaflags} ${archflags} ${includes}")
+  #message(STATUS "All CUDA flags for embed_ptx of ${cu_file}:\n${allcudaflags}")
+  separate_arguments(allcudaflags)
+  #message(STATUS "embed_ptx build command 1: ${CMAKE_CUDA_COMPILER} ${allcudaflags} -ptx ${cu_file_frombin} -o ${ptxfile}")
+  #message(STATUS "embed_ptx build command 2: ${BIN2C} -c --padd 0 --type char --name ${c_embed_name} ${ptxfile} > ${embedfile}")
+  # Ending
+  add_custom_command(
+    OUTPUT ${embedfile}
+    COMMAND echo ${CMAKE_CUDA_COMPILER} ${allcudaflags} -ptx ${cu_file_frombin} -o ${ptxfile}
+    COMMAND ${CMAKE_CUDA_COMPILER} ${allcudaflags} -ptx ${cu_file_frombin} -o ${ptxfile}
+    COMMAND echo ${BIN2C} -c --padd 0 --type char --name ${c_embed_name} ${ptxfile} > ${embedfile}
+    COMMAND ${BIN2C} -c --padd 0 --type char --name ${c_embed_name} ${ptxfile} > ${embedfile}
+    DEPENDS ${cu_file}
+    COMMENT "Building embedded PTX source ${embedfile}"
+    VERBATIM
+  )
+  set(${c_embed_name} ${embedfile} PARENT_SCOPE)
+endfunction()
