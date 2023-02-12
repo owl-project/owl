cd1d93e58fee7e6b615d9e38d9f9b39d96cf648d
Ingo Wald <ingowald@gmail.com>
2023-02-11 19:50:38 -0700

    fixed missing texture read mode; and fixed random number generator to avoid some (rare) cases where it eeturned 1.f

--- owl/TrianglesGeomGroup.cpp
+++ owl/TrianglesGeomGroup.cpp
@@ -1,5 +1,5 @@
 // ======================================================================== //
-// Copyright 2019-2022 Ingo Wald                                            //
+// Copyright 2019-2023 Ingo Wald                                            //
 //                                                                          //
 // Licensed under the Apache License, Version 2.0 (the "License");          //
 // you may not use this file except in compliance with the License.         //
@@ -327,7 +327,7 @@ namespace owl {
       // download builder's compacted size from device
       uint64_t compactedSize;
       compactedSizeBuffer.download(&compactedSize);
-      
+
       if (Context::useManagedMemForAccelData)
         dd.bvhMemory.allocManaged(compactedSize);
       else
--- owl/include/owl/common/math/random.h
+++ owl/include/owl/common/math/random.h
@@ -1,5 +1,5 @@
 // ======================================================================== //
-// Copyright 2018-2020 Ingo Wald                                            //
+// Copyright 2018-2023 Ingo Wald                                            //
 //                                                                          //
 // Licensed under the Apache License, Version 2.0 (the "License");          //
 // you may not use this file except in compliance with the License.         //
@@ -82,14 +82,14 @@ namespace owl {
         state = v0;
       }
     
-      // Generate random unsigned int in [0, 2^24)
+      /*! Generate random unsigned int in [0, 2^24), then use that to
+          generate random float in [0.f,1.f) */
       inline __both__ float operator() ()
       {
         const uint32_t LCG_A = 1664525u;
         const uint32_t LCG_C = 1013904223u;
         state = (LCG_A * state + LCG_C);
-        return ldexpf(float(state), -32);
-        // return (state & 0x00FFFFFF) / (float) 0x01000000;
+        return (state & 0x00FFFFFF) / (float) 0x01000000;
       }
     
       uint32_t state;
