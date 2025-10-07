0f6c129c1b08c017c649d41bdde95aeef59aa061
Ingo Wald <ingowald@gmail.com>
2025-10-06 22:13:37 -0600

    added proper static library linkage flags in both owl and owl_prime

--- prime/include/owl/owl_prime.h
+++ prime/include/owl/owl_prime.h
@@ -20,7 +20,7 @@
 #include <sys/types.h>
 #include <stdint.h>
 
-#ifdef _WIN32
+#if defined(_MSC_VER)
 # if defined(owl_prime_static_STATIC)
 #  define OP_INTERFACE /* nothing */
 # elif defined(owl_prime_EXPORTS)
@@ -38,7 +38,7 @@
 #  define OP_API extern "C" OP_INTERFACE
 #  define OP_IF_CPP(a) a
 #else
-#  define OP_API /* bla */
+#  define OP_API /* nothing */ OP_INTERFACE
 #  define OP_IF_CPP(a) /* ignore */
 #endif
 
